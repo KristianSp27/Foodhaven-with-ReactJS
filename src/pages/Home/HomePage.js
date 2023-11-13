49592cdc7b41bd5697b7e3d9999847458b8801bb
Kristian Spasov <kristian.spasov27@gmail.com>
2023-11-14 01:42:35 +0200

    Update

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,5 +1,15 @@
 import React from "react";
 
+const initialState = { foods: [] };
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FOODS_LOADED":
+      return { ...state, foods: action.payload };
+    default:
+      return state;
+  }
+};
 export default function HomePage() {
   return <div>HomePage</div>;
 }
