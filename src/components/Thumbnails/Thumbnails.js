83e790139b4e3115a23be238e4cf67f9dee8af9e
Kristian Spasov <kristian.spasov27@gmail.com>
2023-11-14 02:46:17 +0200

    Update

--- src/components/Thumbnails/Thumbnails.js
+++ src/components/Thumbnails/Thumbnails.js
@@ -1,5 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import classes from "./thumbnails.module.css";
 
 export default function Thumbnails({ foods }) {
-  return <div>Thumbnails{foods.length}</div>;
+  return (
+    <ul className={classes.list}>
+      {foods.map((food) => (
+        <li key={food.id}>
+          <Link to={`/food/${food.id}`}>
+            <img className={classes.image} src={`/foods/${food.imageUrl}`} alt={food.name} />
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
 }
