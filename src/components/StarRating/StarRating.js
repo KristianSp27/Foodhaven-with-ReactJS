8e7b7910ea107b81c4aa973cb9c4a9964be92787
Kristian Spasov <kristian.spasov27@gmail.com>
2023-11-17 21:48:45 +0200

    Update

--- src/components/StarRating/StarRating.js
+++ src/components/StarRating/StarRating.js
@@ -1,6 +1,11 @@
 import React from "react";
 
 export default function StarRating({ stars, size }) {
+  const styles = {
+    width: size + "px",
+    height: size + "px",
+    marginRight: size / 6 + "px",
+  };
   return <div>StarRating</div>;
 }
 
