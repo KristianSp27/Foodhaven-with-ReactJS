cf5683e8b78fb65f5f3eab1524fd65e173a6819b
Kristian Spasov <kristian.spasov27@gmail.com>
2023-11-17 21:53:48 +0200

    Update

--- src/components/StarRating/StarRating.js
+++ src/components/StarRating/StarRating.js
@@ -9,6 +9,18 @@ export default function StarRating({ stars, size }) {
   return <div>StarRating</div>;
 }
 
+function Star({ number }) {
+  const halfNumber = number - 0.5;
+
+  return stars >= number ? (
+    <img src="/star-full.svg" style={styles} alt={number} />
+  ) : stars >= halfNumber ? (
+    <img src="/star-half.svg" style={styles} alt={number} />
+  ) : (
+    <img src="/star-empty.svg" style={styles} alt={number} />
+  );
+}
+
 StarRating.defaultProps = {
   size: 18,
 };
