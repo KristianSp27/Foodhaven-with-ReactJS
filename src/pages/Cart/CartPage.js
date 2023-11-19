88ec1ff5839809a057f29ebe266ff672b4101a8e
Kristian Spasov <kristian.spasov27@gmail.com>
2023-11-19 22:55:48 +0200

    Update

--- src/pages/Cart/CartPage.js
+++ src/pages/Cart/CartPage.js
@@ -1,8 +1,27 @@
 import React from "react";
 import classes from "./cartPage.module.css";
 import { useCart } from "../../hooks/useCart";
+import Title from "../../components/Title/Title";
 
 export default function CartPage() {
   const { cart } = useCart();
-  return <div>{cart.items.length}</div>;
+  return (
+    <>
+      <Title title="Cart Page" margin="1.5rem 0 0 2.5rem" />
+
+      {cart && cart.items.length > 0 && (
+        <div className={classes.container}>
+          <ul className={classes.list}>
+            {cart.items.map((item) => (
+              <li key={item.food.id}>
+                <div>
+                  <img src={`/foods/${item.food.imageUrl}`} alt={item.food.name} />
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </>
+  );
 }
