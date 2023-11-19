d708780a988aaf8eeb5da3252773d5f46cb2e2d7
Kristian Spasov <kristian.spasov27@gmail.com>
2023-11-19 22:45:36 +0200

    Update

--- src/pages/Cart/CartPage.js
+++ src/pages/Cart/CartPage.js
@@ -1,6 +1,8 @@
 import React from "react";
 import classes from "./cartPage.module.css";
+import { useCart } from "../../hooks/useCart";
 
 export default function CartPage() {
-  return <div>CartPage</div>;
+  const { cart } = useCart();
+  return <div>{cart.items.length}</div>;
 }
