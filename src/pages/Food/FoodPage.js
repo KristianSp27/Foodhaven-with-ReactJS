252836371f8eac22c6264def1b3014654894a681
Kristian Spasov <kristian.spasov27@gmail.com>
2023-11-19 21:32:50 +0200

    Update

--- src/pages/Food/FoodPage.js
+++ src/pages/Food/FoodPage.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import classes from "./foodPage.module.css";
 import { useParams } from "react-router-dom";
 import { getById } from "../../services/foodService";
+import StarRating from "../../components/StarRating/StarRating";
 
 export default function FoodPage() {
   const [food, setFood] = useState({});
@@ -15,6 +16,23 @@ export default function FoodPage() {
       {food && (
         <div className={classes.container}>
           <img className={classes.image} src={`/foods/${food.imageUrl}`} alt={food.name} />
+
+          <div className={classes.details}>
+            <div className={classes.header}>
+              <span className={classes.name}> {food.name} </span>
+              <span className={`${classes.favorite} ${food.favorite ? "" : classes.not}`}>❤</span>
+            </div>
+
+            <div className={classes.rating}>
+              <StarRating stars={food.stars} size={25} />
+            </div>
+
+            <div className={classes.origins}>
+              {food.origins?.map((origin) => (
+                <span key={origin}>{origin}</span>
+              ))}
+            </div>
+          </div>
         </div>
       )}
     </>
