f5d14a62c71536566b9400952938641cfa930859
Kristian Spasov <kristian.spasov27@gmail.com>
2023-11-19 21:05:58 +0200

    Update

--- src/services/foodService.js
+++ src/services/foodService.js
@@ -1,7 +1,41 @@
-import { sample_foods, sample_tags } from "../data";
+import React, { useEffect, useReducer } from "react";
+import { useParams } from "react-router-dom";
+import Search from "../../components/Search/Search";
+import Tags from "../../components/Tags/Tags";
+import Thumbnails from "../../components/Thumbnails/Thumbnails";
+import { getAll, getAllByTag, getAllTags, search } from "../../services/foodService";
 
-export const getAll = async () => sample_foods;
+const initialState = { foods: [], tags: [] };
 
-export const search = async (searchTerm) => sample_foods.filter((item) => item.name.toLowerCase().includes(searchTerm.toLowerCase()));
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FOODS_LOADED":
+      return { ...state, foods: action.payload };
+    case "TAGS_LOADED":
+      return { ...state, tags: action.payload };
+    default:
+      return state;
+  }
+};
 
-export const getAllTags = async () => sample_tags;
+export default function HomePage() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { foods, tags } = state;
+  const { searchTerm, tag } = useParams();
+
+  useEffect(() => {
+    getAllTags().then((tags) => dispatch({ type: "TAGS_LOADED", payload: tags }));
+
+    const loadFoods = tag ? getAllByTag(tag) : searchTerm ? search(searchTerm) : getAll();
+
+    loadFoods.then((foods) => dispatch({ type: "FOODS_LOADED", payload: foods }));
+  }, [searchTerm, tag]);
+
+  return (
+    <>
+      <Search />
+      <Tags tags={tags} />
+      <Thumbnails foods={foods} />
+    </>
+  );
+}
