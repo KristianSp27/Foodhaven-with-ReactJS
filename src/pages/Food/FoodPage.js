8cb3b0554f5990e9a7275636fab8939a446f7a83
Kristian Spasov <kristian.spasov27@gmail.com>
2023-11-19 21:27:47 +0200

    Update

--- src/pages/Food/FoodPage.js
+++ src/pages/Food/FoodPage.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import classes from "./foodPage.module.css";
+import { useParams } from "react-router-dom";
+import { getById } from "../../services/foodService";
 
 export default function FoodPage() {
-  return <div>FoodPage</div>;
+  const [food, setFood] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    getById(id).then(setFood);
+  }, [id]);
+  return (
+    <>
+      {food && (
+        <div className={classes.container}>
+          <img className={classes.image} src={`/foods/${food.imageUrl}`} alt={food.name} />
+        </div>
+      )}
+    </>
+  );
 }
