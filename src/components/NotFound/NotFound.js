81ec666fd7121faa961b11e67c96701e9ae9d1e6
Kristian Spasov <kristian.spasov27@gmail.com>
2023-11-21 23:27:58 +0200

    Update

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,6 +1,12 @@
 import React from "react";
 import classes from "./notFound.module.css";
+import { Link } from "react-router-dom";
 
-export default function NotFound() {
-  return <div>NotFound</div>;
+export default function NotFound(message, linkRoute, linkText) {
+  return (
+    <div className={classes.container}>
+      {message}
+      <Link to={linkRoute}>{linkText}</Link>
+    </div>
+  );
 }
