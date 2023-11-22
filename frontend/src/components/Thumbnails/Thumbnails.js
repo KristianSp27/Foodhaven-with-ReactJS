c8a6d8616916e316beeb7a53b199dbc5ebbe058e
Kristian Spasov <kristian.spasov27@gmail.com>
2023-11-23 00:03:49 +0200

    Update

--- frontend/src/components/Thumbnails/Thumbnails.js
+++ frontend/src/components/Thumbnails/Thumbnails.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import classes from "./thumbnails.module.css";
-import StarRating from "../StarRating/StarRating";
 import Price from "../Price/Price";
-
+import StarRating from "../StarRating/StarRating";
+import classes from "./thumbnails.module.css";
 export default function Thumbnails({ foods }) {
   return (
     <ul className={classes.list}>
       {foods.map((food) => (
         <li key={food.id}>
           <Link to={`/food/${food.id}`}>
-            <img className={classes.image} src={`/foods/${food.imageUrl}`} alt={food.name} />
+            <img className={classes.image} src={`${food.imageUrl}`} alt={food.name} />
+
             <div className={classes.content}>
               <div className={classes.name}>{food.name}</div>
               <span className={`${classes.favorite} ${food.favorite ? "" : classes.not}`}>❤</span>
