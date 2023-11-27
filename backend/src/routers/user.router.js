80bb959b5e1409cf07c9a341d32c2d5ba14837a5
Kristian Spasov <kristian.spasov27@gmail.com>
2023-11-27 23:23:09 +0200

    Update

--- backend/src/routers/user.router.js
+++ backend/src/routers/user.router.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
+import { sample_users } from "../data";
 
 const router = Router();
 
 router.post("/login", (req, res) => {
   const { email, password } = req.body;
+  const user = sample_users.find((user) => user.email === email && user.password === password);
 });
