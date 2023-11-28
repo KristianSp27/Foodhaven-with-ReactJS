c10950cd514d4f31999fc9d44a955ea71f9b4da7
Kristian Spasov <kristian.spasov27@gmail.com>
2023-11-28 21:14:36 +0200

    Update

--- frontend/src/pages/Login/LoginPage.js
+++ frontend/src/pages/Login/LoginPage.js
@@ -1,5 +1,19 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { useAuth } from "../../hooks/useAuth";
 
 export default function LoginPage() {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
+
+  const navigate = useNavigate();
+  const { user, login } = useAuth();
+  const [params] = useSearchParams();
+  const returnUrl = params.get("returnUrl");
+
   return <div>LoginPage</div>;
 }
