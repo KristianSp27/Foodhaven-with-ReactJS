03c4a76656c1d94ad9717e6757985c815e226ce3
Kristian Spasov <kristian.spasov27@gmail.com>
2023-11-28 20:51:49 +0200

    Update

---
+++ frontend/src/hooks/useAuth.js
@@ -0,0 +1,19 @@
+import { useState, createContext, useContext } from "react";
+import * as userService from "../services/userService";
+import { toast } from "react-toastify";
+
+const AuthContext = createContext(null);
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(userService.getUser());
+
+  const login = async (email, password) => {
+    try {
+      const user = await userService.login(email, password);
+      setUser(user);
+      toast.success("Login successful!");
+    } catch (err) {
+      toast.error(err.response.data);
+    }
+  };
+};
