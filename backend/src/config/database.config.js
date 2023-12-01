ab968ffc7a29c29940e44ef794f63f978f010a41
Kristian Spasov <kristian.spasov27@gmail.com>
2023-12-02 01:09:43 +0200

    Update

--- backend/src/config/database.config.js
+++ backend/src/config/database.config.js
@@ -16,6 +16,7 @@ export const dbConnect = async () => {
       useUnifiedTopology: true,
     });
     await seedUsers();
+    await seedFoods();
     console.log("connected successfully");
   } catch (error) {
     console.log(error);
