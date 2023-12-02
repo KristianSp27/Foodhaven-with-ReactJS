adab776e35ff2d90af43fe245e7be0be167f963c
Kristian Spasov <kristian.spasov27@gmail.com>
2023-12-02 21:29:24 +0200

    Update RegisterPage.js
    Update

--- frontend/src/pages/Register/RegisterPage.js
+++ frontend/src/pages/Register/RegisterPage.js
@@ -1,5 +1,25 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import Input from "../../components/Input/Input";
+import Title from "../../components/Title/Title";
+import classes from "./registerPage.module.css";
+import Button from "../../components/Button/Button";
+import { Link } from "react-router-dom";
 
 export default function RegisterPage() {
-  return <div>RegisterPage</div>;
+  const {
+    handleSubmit,
+    register,
+    getValues,
+    formState: { errors },
+  } = useForm();
+
+  const submit = async (data) => {};
+  return (
+    <div className={classes.container}>
+      <div className={classes.details}>
+        <Title title="Register" />
+      </div>
+    </div>
+  );
 }
