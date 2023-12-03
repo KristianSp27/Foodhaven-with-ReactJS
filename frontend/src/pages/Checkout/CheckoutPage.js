b3b309acab6a076ea75fa8dd5c43ce0065ef1807
Kristian Spasov <kristian.spasov27@gmail.com>
2023-12-03 23:10:55 +0200

    Update

--- frontend/src/pages/Checkout/CheckoutPage.js
+++ frontend/src/pages/Checkout/CheckoutPage.js
@@ -1,5 +1,13 @@
 import React from "react";
+import { useCart } from "../../hooks/useCart";
+import { useAuth } from "../../hooks/useAuth";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function CheckoutPage() {
+  const { cart } = useCart();
+  const { user } = useAuth();
+  const navigate = useNavigate();
+  const [order, setOrder] = useState({ ...cart });
   return <div>CheckoutPage</div>;
 }
