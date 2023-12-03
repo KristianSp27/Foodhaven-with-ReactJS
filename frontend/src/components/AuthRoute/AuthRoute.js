fc64a0f686836de7e87da0c6195ba048fd11c9e8
Kristian Spasov <kristian.spasov27@gmail.com>
2023-12-03 23:02:14 +0200

    Update

--- frontend/src/components/AuthRoute/AuthRoute.js
+++ frontend/src/components/AuthRoute/AuthRoute.js
@@ -2,5 +2,7 @@ import { Navigate, useLocation } from "react-router-dom";
 import { useAuth } from "../../hooks/useAuth";
 
 export default function AuthRoute() {
-  return <div>AuthRoute</div>;
+  const location = useLocation();
+  const { user } = useAuth();
+  return user ? children : <Navigate to={`/login?returnUrl=${location.pathname}`} replace />;
 }
