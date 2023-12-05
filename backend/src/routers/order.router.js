f03097bc287e342be329ff3b712b27754fdb4292
Kristian Spasov <kristian.spasov27@gmail.com>
2023-12-06 00:05:54 +0200

    Update

--- backend/src/routers/order.router.js
+++ backend/src/routers/order.router.js
@@ -1,4 +1,16 @@
 import { Router } from "express";
 import handler from "express-async-handler";
+import authMid from "../middleware/auth.mid";
+import { BAD_REQUEST } from "../constants/httpStatus";
 
 const router = Router();
+router.use(auth);
+
+router.post(
+  "/create",
+  handler(async (req, res) => {
+    const order = req.body;
+
+    if (order.items.length <= 0) res.status(BAD_REQUEST).send("The cart is empty!");
+  })
+);
