1d1aeab10b992d5d95a52b8675c89232cb84b526
Kristian Spasov <kristian.spasov27@gmail.com>
2023-12-05 23:54:11 +0200

    Update

--- frontend/src/pages/Checkout/CheckoutPage.js
+++ frontend/src/pages/Checkout/CheckoutPage.js
@@ -61,6 +61,7 @@ export default function CheckoutPage() {
           <Map
             location={order.addressLatLng}
             onChange={(latlng) => {
+              console.log(latlng);
               setOrder({ ...order, addressLatLng: latlng });
             }}
           />
