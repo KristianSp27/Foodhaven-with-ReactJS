d05c9f1b6253cabb8892946b1280a1458f7764e0
Kristian Spasov <kristian.spasov27@gmail.com>
2023-12-05 23:24:16 +0200

    Update

--- frontend/src/components/Map/Map.js
+++ frontend/src/components/Map/Map.js
@@ -5,5 +5,20 @@ import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from "react-leaf
 import { toast } from "react-toastify";
 
 export default function Map({ readOnly, location, onChange }) {
-  return <div>Map</div>;
+  return (
+    <div className={classes.container}>
+      <MapContainer
+        className={classes.map}
+        center={[0, 0]}
+        zoom={1}
+        dragging={!readOnly}
+        touchZoom={!readOnly}
+        doubleClickZoom={!readOnly}
+        scrollWheelZoom={!readOnly}
+        boxZoom={!readOnly}
+        keyboard={!readOnly}
+        attributionControl={!readOnly}
+      ></MapContainer>
+    </div>
+  );
 }
