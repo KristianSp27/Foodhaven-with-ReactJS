2e5c9e58befb73153984aabcb83851c98312c716
Kristian Spasov <kristian.spasov27@gmail.com>
2023-12-06 00:18:44 +0200

    Update

--- backend/src/models/order.model.js
+++ backend/src/models/order.model.js
@@ -1,6 +1,7 @@
 import { model, Schema } from "mongoose";
 import { OrderStatus } from "../constants/orderStatus.js";
 import { FoodModel } from "./food.model.js";
+import { BAD_REQUEST } from "../constants/httpStatus.js";
 
 export const LatLngSchema = new Schema(
   {
