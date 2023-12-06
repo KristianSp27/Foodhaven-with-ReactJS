481b0ed21fbc711d82790b7e581557ef7b80b479
kristian <kristian.spasov27@gmail.com>
2023-12-07 00:39:13 +0200

    Correction

--- backend/src/config/database.config.js
+++ backend/src/config/database.config.js
@@ -15,7 +15,7 @@ export const dbconnect = async () => {
     });
     await seedUsers();
     await seedFoods();
-    console.log("connect successfully---");
+    console.log("Connected successfully---");
   } catch (error) {
     console.log(error);
   }
