7bcec3c1590485cf1982b391dbd77d63de694bb4
kristian <kristian.spasov27@gmail.com>
2023-12-08 01:13:21 +0200

    Update

--- backend/src/routers/order.router.js
+++ backend/src/routers/order.router.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import handler from "express-async-handler";
 import auth from "../middleware/auth.mid.js";
-import { BAD_REQUEST } from "../constants/httpStatus.js";
+import { BAD_REQUEST, UNAUTHORIZED } from "../constants/httpStatus.js";
 import { OrderModel } from "../models/order.model.js";
 import { OrderStatus } from "../constants/orderStatus.js";
 import { UserModel } from "../models/user.model.js";
@@ -51,6 +51,20 @@ router.get(
   handler(async (req, res) => {
     const { orderId } = req.params;
     const user = await UserModel.findById(req.user.id);
+
+    const filter = {
+      _id: orderId,
+    };
+
+    if (!user.admin) {
+      filter.user = user._id;
+    }
+
+    const order = await OrderModel.findOne(filter);
+
+    if (!order) return res.send(UNAUTHORIZED);
+
+    return res.send(order);
   })
 );
 
