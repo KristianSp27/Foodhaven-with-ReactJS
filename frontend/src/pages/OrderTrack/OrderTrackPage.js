ba36202bfb2893819482579dcf8970fb8112e3f2
kristian <kristian.spasov27@gmail.com>
2023-12-08 01:17:28 +0200

    Update

--- frontend/src/pages/OrderTrack/OrderTrackPage.js
+++ frontend/src/pages/OrderTrack/OrderTrackPage.js
@@ -1,5 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { trackOrderById } from "../../services/orderService";
+import NotFound from "../../components/NotFound/NotFound";
 
 export default function OrderTrackPage() {
+  const { orderId } = useParams();
+  const [order, setOrder] = useState();
+
+  useEffect(() => {
+    orderId &&
+      trackOrderById(orderId).then((order) => {
+        setOrder(order);
+      });
+  }, []);
+
+  if (!orderId) return <NotFound message="Order not found!" linkText="Go to the homepage" />;
   return <div>OrderTrackPage</div>;
 }
