96baafcf6f23dfcdb668adfbfd6e6a74768130d9
kristian <kristian.spasov27@gmail.com>
2023-12-08 01:44:07 +0200

    Update

--- frontend/src/components/Map/Map.js
+++ frontend/src/components/Map/Map.js
@@ -3,6 +3,7 @@ import classes from "./map.module.css";
 import "leaflet/dist/leaflet.css";
 import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from "react-leaflet";
 import { toast } from "react-toastify";
+import * as L from "leaflet";
 
 export default function Map({ readonly, location, onChange }) {
   return (
@@ -50,11 +51,18 @@ function FindButtonAndMarker({ readonly, location, onChange }) {
     },
   });
 
+  const markerIcon = new L.Icon({
+    iconUrl: "/marker-icon-2x.png",
+    iconSize: [25, 41],
+    iconAnchor: [12.5, 41],
+    popupAnchor: [0, -41],
+  });
+
   return (
     <>
       {!readonly && (
         <button type="button" className={classes.find_location} onClick={() => map.locate()}>
-          Find my location
+          Find My Location
         </button>
       )}
 
@@ -67,6 +75,7 @@ function FindButtonAndMarker({ readonly, location, onChange }) {
           }}
           position={position}
           draggable={!readonly}
+          icon={markerIcon}
         >
           <Popup>Shipping Location</Popup>
         </Marker>
