7288f5263e174a47b7ce968c7bfd980295b6264e
kristian <kristian.spasov27@gmail.com>
2023-12-07 16:02:02 +0200

    Update

--- frontend/src/pages/Payment/PaymentPage.js
+++ frontend/src/pages/Payment/PaymentPage.js
@@ -1,7 +1,23 @@
 import React, { useState, useEffect } from "react";
 import classes from "./paymentPage.module.css";
+import { getNewOrderForCurrentUser } from "../../services/orderService";
+import Title from "../../components/Title/Title";
 
 export default function PaymentPage() {
   const [order, seetOrder] = useState();
-  return <div>PaymentPage</div>;
+
+  useEffect(() => {
+    getNewOrderForCurrentUser().then((data) => seetOrder(data));
+  }, []);
+
+  if (!order) return;
+  return (
+    <>
+      <div className={classes.container}>
+        <div className={classes.content}>
+          <Title title="Order form" fontSize="1.6rem" />
+        </div>
+      </div>
+    </>
+  );
 }
