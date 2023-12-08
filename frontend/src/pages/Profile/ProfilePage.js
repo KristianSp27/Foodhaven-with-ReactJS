fe2e34e0c56c67d32d4479b5c4f181dc05852d04
kristian <kristian.spasov27@gmail.com>
2023-12-08 13:43:15 +0200

    Update

--- frontend/src/pages/Profile/ProfilePage.js
+++ frontend/src/pages/Profile/ProfilePage.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import { useAuth } from "../../hooks/useAuth";
+import classes from ".profilePage.module.css/";
+import Title from "../../components/Title/Title";
+import Input from "../../components/Input/Input";
 
 export default function ProfilePage() {
   const {
@@ -14,5 +17,23 @@ export default function ProfilePage() {
   const submit = (user) => {
     //updates the profile
   };
-  return <div>ProfilePage</div>;
+  return (
+    <div className={classes.container}>
+      <div className={classes.details}>
+        <Title title="Update profile" />
+        <form onSubmit={handleSubmit(submit)}>
+          <Input
+            defaultValue={user.name}
+            type="text"
+            label="name"
+            {...register("name", {
+              required: true,
+              minLength: 5,
+            })}
+            error={errors.name}
+          />
+        </form>
+      </div>
+    </div>
+  );
 }
