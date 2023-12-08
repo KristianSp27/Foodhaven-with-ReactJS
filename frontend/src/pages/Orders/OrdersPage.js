f6383f2ef0b20c22f53b67c55cdf3b5d6157e119
kristian <kristian.spasov27@gmail.com>
2023-12-09 00:25:18 +0200

    Update

--- frontend/src/pages/Orders/OrdersPage.js
+++ frontend/src/pages/Orders/OrdersPage.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useReducer } from "react";
 import { useParams } from "react-router-dom";
 import { getAll } from "../../services/orderService";
+import classes from "./ordersPage.module.css";
+import Title from "../../components/Title/Title";
+import DateTime from "../../components/DateTime/DateTime";
 
 const initialState = {};
 
@@ -23,5 +26,22 @@ export default function OrdersPage() {
       dispatch({ type: "ORDERS_FETCHED", payload: orders });
     });
   }, [filter]);
-  return <div>OrdersPage</div>;
+  return (
+    <div className={classes.container}>
+      <Title title="Orders" margin="1.5rem 0 0 .2rem" fontSize="1.9rem" />
+
+      {orders &&
+        orders.map((order) => (
+          <div key={order.id} className={classes.order_summary}>
+            <div className={classes.header}>
+              <span>{order.id}</span>
+              <span>
+                <DateTime date={order.createdAt} />
+              </span>
+              <span>{order.status}</span>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
 }
