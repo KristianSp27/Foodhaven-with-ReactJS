90287a450a61883892e12fef14a9d83a0b3dee75
kristian <kristian.spasov27@gmail.com>
2024-03-26 23:28:26 +0200

    Update

--- frontend/src/pages/Orders/OrdersPage.js
+++ frontend/src/pages/Orders/OrdersPage.js
@@ -1,5 +1,14 @@
 import React from "react";
 
+const reducer = (state, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    default:
+      return state;
+  }
+};
+const initialState = {};
+
 export default function OrdersPage() {
   return <div>OrdersPage</div>;
 }
