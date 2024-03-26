144aafdee5c924f56d5d94be060fa24ae6e729c0
kristian <kristian.spasov27@gmail.com>
2024-03-26 23:40:49 +0200

    Adding orders page route

--- frontend/src/AppRoutes.js
+++ frontend/src/AppRoutes.js
@@ -54,6 +54,14 @@ export default function AppRoutes() {
           </AuthRoute>
         }
       />
+      <Route
+        path="/orders"
+        element={
+          <AuthRoute>
+            <OrdersPage />
+          </AuthRoute>
+        }
+      />
     </Routes>
   );
 }
