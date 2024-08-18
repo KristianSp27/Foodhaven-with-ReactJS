e4d0c6c1b30fea6910e68e0aed3bc59c2b40ac1e
kristian <kristian.spasov27@gmail.com>
2024-08-18 23:58:56 +0300

    Update

--- frontend/src/pages/Dashboard/Dashboard.js
+++ frontend/src/pages/Dashboard/Dashboard.js
@@ -4,5 +4,42 @@ import classes from "./dashboard.module.css";
 
 export default function Dashboard() {
   const { user } = useAuth();
-  return <div className={classes.container}>Dashboard</div>;
+  return (
+    <div className={classes.container}>
+      <div className={classes.menu}></div>
+    </div>
+  );
 }
+
+const allItems = [
+  {
+    title: "Orders",
+    imageUrl: "/icons/orders.svg",
+    url: "/orders",
+    bgColor: "#ec407a",
+    color: "white",
+  },
+  {
+    title: "Profile",
+    imageUrl: "/icons/profile.svg",
+    url: "/profile",
+    bgColor: "#1565c0",
+    color: "white",
+  },
+  {
+    title: "Users",
+    imageUrl: "/icons/users.svg",
+    url: "/admin/users",
+    forAdmin: true,
+    bgColor: "#00bfa5",
+    color: "white",
+  },
+  {
+    title: "Foods",
+    imageUrl: "/icons/foods.svg",
+    url: "/admin/foods",
+    forAdmin: true,
+    bgColor: "#e040fb",
+    color: "white",
+  },
+];
