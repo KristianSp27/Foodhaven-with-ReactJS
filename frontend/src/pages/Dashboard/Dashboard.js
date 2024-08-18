2c067a9be3b85931c823694ca6665709f9bb0d02
kristian <kristian.spasov27@gmail.com>
2024-08-18 23:56:36 +0300

    Update

--- frontend/src/pages/Dashboard/Dashboard.js
+++ frontend/src/pages/Dashboard/Dashboard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useAuth } from "../../hooks/useAuth";
+import classes from "./dashboard.module.css";
 
 export default function Dashboard() {
   const { user } = useAuth();
