6e083d677d40595c467a741343a8c49b665d37df
kristian <kristian.spasov27@gmail.com>
2024-08-19 23:46:07 +0300

    Adding image to the dashboar dmenu option.

--- frontend/src/pages/Dashboard/Dashboard.js
+++ frontend/src/pages/Dashboard/Dashboard.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { useAuth } from "../../hooks/useAuth";
 import classes from "./dashboard.module.css";
+import { Link } from "react-router-dom";
 
 export default function Dashboard() {
   const { user } = useAuth();
   return (
     <div className={classes.container}>
-      <div className={classes.menu}></div>
+      <div className={classes.menu}>
+        {allItems
+          .filter((item) => user.isAdmin || !item.forAdmin)
+          .map((item) => (
+            <Link to={item.url} style={{ backgroundColor: item.backgroundColor, color: item.color }}>
+              <img src={item.imageUrl} alt={item.title} />
+            </Link>
+          ))}
+      </div>
     </div>
   );
 }
