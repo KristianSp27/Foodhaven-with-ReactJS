51b314877aa3cd043cdacffdb1bda98e554819c7
kristian <kristian.spasov27@gmail.com>
2024-09-26 22:59:11 +0300

    Adding search in the admin page

--- frontend/src/pages/FoodsAdmin/FoodsAdminPage.js
+++ frontend/src/pages/FoodsAdmin/FoodsAdminPage.js
@@ -1,9 +1,15 @@
 import { useState } from "react";
 import classes from "./foodsAdminPage.module.css";
 import { useParams } from "react-router-dom";
+import { getAll, search } from "../../services/foodService";
 
 export default function FoodsAdminPage() {
   const [foods, setFoods] = useState();
   const { searchTerm } = useParams();
+
+  const loadFoods = async () => {
+    const foods = searchTerm ? await search(searchTerm) : await getAll();
+    setFoods(foods);
+  };
   return <div>FoodsAdminPage</div>;
 }
