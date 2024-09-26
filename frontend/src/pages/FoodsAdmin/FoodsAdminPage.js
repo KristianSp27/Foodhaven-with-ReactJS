29a428114c1c29fe2f4dcf28e60db666a383bf83
kristian <kristian.spasov27@gmail.com>
2024-09-26 22:57:15 +0300

    Update

--- frontend/src/pages/FoodsAdmin/FoodsAdminPage.js
+++ frontend/src/pages/FoodsAdmin/FoodsAdminPage.js
@@ -1,5 +1,9 @@
+import { useState } from "react";
 import classes from "./foodsAdminPage.module.css";
+import { useParams } from "react-router-dom";
 
 export default function FoodsAdminPage() {
+  const [foods, setFoods] = useState();
+  const { searchTerm } = useParams();
   return <div>FoodsAdminPage</div>;
 }
