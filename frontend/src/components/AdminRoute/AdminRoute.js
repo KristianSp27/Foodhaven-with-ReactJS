2b268cce1a9a5c0aeb108b78fbf5c3ea1795f71d
kristian <kristian.spasov27@gmail.com>
2024-09-26 22:42:30 +0300

    Update

--- frontend/src/components/AdminRoute/AdminRoute.js
+++ frontend/src/components/AdminRoute/AdminRoute.js
@@ -1,5 +1,12 @@
 import React from "react";
+import { useAuth } from "../../hooks/useAuth";
+import NotFound from "../NotFound/NotFound";
 
-function AdminRoute() {
-  return <div>AdminRoute</div>;
+function AdminRoute({ children }) {
+  const { user } = useAuth();
+  return user.isAdmin ? (
+    children
+  ) : (
+    <NotFound linkRoute="/dashboard" linkText="Go to the dashboard" message="You ddon't have access to this page."></NotFound>
+  );
 }
