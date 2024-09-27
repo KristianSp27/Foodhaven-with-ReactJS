7a7aa926aec81aba4b3f6130907504b4a645932c
kristian <kristian.spasov27@gmail.com>
2024-09-27 23:46:45 +0300

    Update

--- backend/src/middleware/admin.mid.js
+++ backend/src/middleware/admin.mid.js
@@ -1,6 +1,5 @@
-import { UNAUTHORIZED } from "../constants/httpStatus";
-import authMid from "./auth.mid";
-
+import { UNAUTHORIZED } from "../constants/httpStatus.js";
+import authMid from "./auth.mid.js";
 const adminMid = (req, res, next) => {
   if (!req.user.isAdmin) res.status(UNAUTHORIZED).send();
 
--- frontend/src/pages/FoodsAdmin/FoodsAdminPage.js
+++ frontend/src/pages/FoodsAdmin/FoodsAdminPage.js
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import classes from "./foodsAdminPage.module.css";
 import { Link, useParams } from "react-router-dom";
-import { getAll, search } from "../../services/foodService";
+import { deleteById, getAll, search } from "../../services/foodService";
 import NotFound from "../../components/NotFound/NotFound";
 import Title from "../../components/Title/Title";
+import Search from "../../components/Search/Search";
 import Price from "../../components/Price/Price";
+import { toast } from "react-toastify";
 
 export default function FoodsAdminPage() {
   const [foods, setFoods] = useState();
@@ -22,18 +24,25 @@ export default function FoodsAdminPage() {
   const FoodsNotFound = () => {
     if (foods && foods.length > 0) return;
 
-    return searchTerm ? (
-      <NotFound linkRoute="/admin/foods" linkText="Show all" />
-    ) : (
-      <NotFound linkRoute="/dashboard" linkText="Return to the dashboard" />
-    );
+    return searchTerm ? <NotFound linkRoute="/admin/foods" linkText="Show All" /> : <NotFound linkRoute="/dashboard" linkText="Back to dashboard!" />;
   };
+
+  const deleteFood = async (food) => {
+    const confirmed = window.confirm(`Delete Food ${food.name}?`);
+    if (!confirmed) return;
+
+    await deleteById(food.id);
+    toast.success(`"${food.name}" has Been Removed!`);
+    setFoods(foods.filter((f) => f.id !== food.id));
+  };
+
   return (
     <div className={classes.container}>
       <div className={classes.list}>
         <Title title="Manage Foods" margin="1rem auto" />
+        <Search searchRoute="/admin/foods/" defaultRoute="/admin/foods" margin="1rem 0" placeholder="Search Foods" />
         <Link to="/admin/addFood" className={classes.add_food}>
-          Add Food
+          Add Food +
         </Link>
         <FoodsNotFound />
         {foods &&
@@ -44,7 +53,7 @@ export default function FoodsAdminPage() {
               <Price price={food.price} />
               <div className={classes.actions}>
                 <Link to={"/admin/editFood/" + food.id}>Edit</Link>
-                <Link>Delete</Link>
+                <Link onClick={() => deleteFood(food)}>Delete</Link>
               </div>
             </div>
           ))}
