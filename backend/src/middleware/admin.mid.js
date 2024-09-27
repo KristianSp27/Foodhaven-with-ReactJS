c85b1730c2d24b2d3239c442d66560bbe7e328a1
kristian <kristian.spasov27@gmail.com>
2024-09-27 23:40:04 +0300

    Update

--- backend/src/middleware/admin.mid.js
+++ backend/src/middleware/admin.mid.js
@@ -2,7 +2,7 @@ import { UNAUTHORIZED } from "../constants/httpStatus";
 import authMid from "./auth.mid";
 
 const adminMid = (req, res, next) => {
-  if (!req.user.isAdmin) res.status(UNAUTHORIZED);
+  if (!req.user.isAdmin) res.status(UNAUTHORIZED).send();
 
   return next();
 };
