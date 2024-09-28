0f7817d16a620f997b8970e8ae91dd0b76454742
kristian <kristian.spasov27@gmail.com>
2024-09-28 21:41:22 +0300

    Adding the form with useForm hook

--- frontend/src/pages/FoodEdit/FoodEditPage.js
+++ frontend/src/pages/FoodEdit/FoodEditPage.js
@@ -1,8 +1,16 @@
 import { useParams } from "react-router-dom";
 import classes from "./foodEdit.module.css";
+import { useForm } from "react-hook-form";
 
 export default function FoodEditPage() {
   const { foodId } = useParams();
   const isEditMode = !!foodId;
+
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+    reset,
+  } = useForm();
   return <div>FoodEditPage</div>;
 }
