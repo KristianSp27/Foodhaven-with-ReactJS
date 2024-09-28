775daf464359353c5ec42a29dc472ff6390e9d26
kristian <kristian.spasov27@gmail.com>
2024-09-28 21:39:57 +0300

    Update

--- frontend/src/pages/FoodEdit/FoodEditPage.js
+++ frontend/src/pages/FoodEdit/FoodEditPage.js
@@ -1,5 +1,8 @@
+import { useParams } from "react-router-dom";
 import classes from "./foodEdit.module.css";
 
 export default function FoodEditPage() {
+  const { foodId } = useParams();
+  const isEditMode = !!foodId;
   return <div>FoodEditPage</div>;
 }
