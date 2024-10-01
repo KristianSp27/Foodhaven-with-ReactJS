27113108620c08204732c20348a16c87e8e99dcf
kristian <kristian.spasov27@gmail.com>
2024-10-02 00:22:25 +0300

    Update

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,15 @@
 import dotenv from "dotenv";
 dotenv.config();
 import { fileURLToPath } from "url";
-
 import express from "express";
 import cors from "cors";
 import foodRouter from "./routers/food.router.js";
 import userRouter from "./routers/user.router.js";
 import orderRouter from "./routers/order.router.js";
+// import uploadRouter from "./routers/upload.router.js";
 
 import { dbconnect } from "./config/database.config.js";
-import { dirname } from "path";
+import path, { dirname } from "path";
 dbconnect();
 
 const __filename = fileURLToPath(import.meta.url);
@@ -27,6 +27,7 @@ app.use(
 app.use("/api/foods", foodRouter);
 app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);
+// app.use("/api/upload", uploadRouter);
 
 const publicFolder = path.join(__dirname, "public");
 app.use(express.static(publicFolder));
@@ -36,7 +37,7 @@ app.get("*", (req, res) => {
   res.sendFile(indexFilePath);
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("listening on port " + PORT);
 });
