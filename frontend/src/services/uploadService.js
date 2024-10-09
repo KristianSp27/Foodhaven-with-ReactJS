fa82cf2f145562ec0aa44aa880f5cf3fe080b65a
kristian <kristian.spasov27@gmail.com>
2024-10-09 03:16:43 +0300

    Update

--- frontend/src/services/uploadService.js
+++ frontend/src/services/uploadService.js
@@ -1,3 +1,9 @@
 export const uploadImage = async (event) => {
   let toastId = null;
+
+  const image = await getImage(event);
+};
+
+const getImage = async (event) => {
+  const files = event.target.files;
 };
