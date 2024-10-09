744e9c7129d1aed336568df456391865de51820b
kristian <kristian.spasov27@gmail.com>
2024-10-09 03:19:36 +0300

    Update

--- frontend/src/services/uploadService.js
+++ frontend/src/services/uploadService.js
@@ -1,3 +1,5 @@
+import { toast } from "react-toastify";
+
 export const uploadImage = async (event) => {
   let toastId = null;
 
@@ -6,4 +8,11 @@ export const uploadImage = async (event) => {
 
 const getImage = async (event) => {
   const files = event.target.files;
+
+  if (!files || files.length <= 0) {
+    toast.warning("Upload file is not selected!", "File upload");
+    return null;
+  }
+
+  const file = files[0];
 };
