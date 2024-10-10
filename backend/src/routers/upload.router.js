66e406a4fbf54be726eb626f8d229f43ff4dfade
kristian <kristian.spasov27@gmail.com>
2024-10-10 23:43:38 +0300

    Update

--- backend/src/routers/upload.router.js
+++ backend/src/routers/upload.router.js
@@ -1,2 +1,8 @@
 import { Router } from "express";
 import admin from "../middleware/admin.mid";
+import multer from "multer";
+
+const router = Router();
+const upload = multer();
+
+router.post("/", admin, upload.single("image"));
