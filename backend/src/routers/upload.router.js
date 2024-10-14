bcdcf59fcd1aece2648d43f2a6f81b75d9409386
kristian <kristian.spasov27@gmail.com>
2024-10-15 01:12:51 +0300

    Update

--- backend/src/routers/upload.router.js
+++ backend/src/routers/upload.router.js
@@ -6,4 +6,11 @@ import handler from "express-async-handler";
 const router = Router();
 const upload = multer();
 
-router.post("/", admin, upload.single("image"));
+router.post(
+  "/",
+  admin,
+  upload.single("image"),
+  handler(async (req, res) => {
+    const file = req.file;
+  })
+);
