cc29b20b851d9a728137548036bb3e7e33b09d06
kristian <kristian.spasov27@gmail.com>
2024-10-16 00:26:01 +0300

    Upload

--- backend/src/config/cloudinary.config.js
+++ backend/src/config/cloudinary.config.js
@@ -1,7 +1,10 @@
 import { v2 as cloudinary } from "cloudinary";
 
-cloudinary.config({
-  cloud_name: proccess.env.CLOUDINARY_CLOUD_NAME,
-  api_key: proccess.env.CLOUDINARY_API_KEY,
-  api_secret: proccess.env.CLOUDINARY_API_SECRET,
-});
+export const configCloudinary = () => {
+  cloudinary.config({
+    cloud_name: proccess.env.CLOUDINARY_CLOUD_NAME,
+    api_key: proccess.env.CLOUDINARY_API_KEY,
+    api_secret: proccess.env.CLOUDINARY_API_SECRET,
+  });
+  return cloudinary;
+};
