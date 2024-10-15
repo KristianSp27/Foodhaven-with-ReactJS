203fc519a135c676ecd1627e13eb15973210e6dc
kristian <kristian.spasov27@gmail.com>
2024-10-16 00:24:33 +0300

    Update

--- backend/src/config/cloudinary.config.js
+++ backend/src/config/cloudinary.config.js
@@ -1,7 +1,7 @@
 import { v2 as cloudinary } from "cloudinary";
 
 cloudinary.config({
-  cloud_name: "dmzksftcn",
-  api_key: "569147636728553",
-  api_secret: "t83EYlRZqjIIVgQdpBzxfRD2Uck",
+  cloud_name: proccess.env.CLOUDINARY_CLOUD_NAME,
+  api_key: proccess.env.CLOUDINARY_API_KEY,
+  api_secret: proccess.env.CLOUDINARY_API_SECRET,
 });
