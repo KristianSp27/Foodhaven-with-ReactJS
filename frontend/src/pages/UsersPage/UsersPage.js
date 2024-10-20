aa144cf00c95f956fa1c2a02f919c81c23c6e355
kristian <kristian.spasov27@gmail.com>
2024-10-21 00:52:30 +0300

    Correction

--- frontend/src/pages/UsersPage/UsersPage.js
+++ frontend/src/pages/UsersPage/UsersPage.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 
 export default function UsersPage() {
-  cosnt[(users, setUsers)] = useState();
+  const [users, setUsers] = useState();
   const { searchTerm } = useParams();
   return <div>UsersPage</div>;
 }
