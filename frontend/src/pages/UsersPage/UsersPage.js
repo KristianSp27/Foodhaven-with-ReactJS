81a508e7dc204ded9100baf65cd77ba67c593700
kristian <kristian.spasov27@gmail.com>
2024-10-21 00:52:03 +0300

    Update

--- frontend/src/pages/UsersPage/UsersPage.js
+++ frontend/src/pages/UsersPage/UsersPage.js
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
 
 export default function UsersPage() {
+  cosnt[(users, setUsers)] = useState();
+  const { searchTerm } = useParams();
   return <div>UsersPage</div>;
 }
