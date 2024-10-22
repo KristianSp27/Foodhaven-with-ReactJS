8906d70884af116087716e9d9f21affa6ed2f883
kristian <kristian.spasov27@gmail.com>
2024-10-22 23:07:21 +0300

    Update

--- frontend/src/pages/UsersPage/UsersPage.js
+++ frontend/src/pages/UsersPage/UsersPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useAuth } from "../../hooks/useAuth";
 import { getAll } from "../../services/userService";
@@ -6,6 +6,10 @@ import { getAll } from "../../services/userService";
 export default function UsersPage() {
   const [users, setUsers] = useState();
   const { searchTerm } = useParams();
+
+  useEffect(() => {
+    loadUsers();
+  }, [searchTerm]);
 
   const loadUsers = async () => {
     const users = await getAll(searchTerm);
