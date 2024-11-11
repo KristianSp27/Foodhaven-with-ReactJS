e09b7abe567850d023b4cbaf77103f5148fa435b
Kristian Spasov <kristian.spasov27@gmail.com>
2024-11-11 23:26:01 +0200

    Update

--- frontend/src/pages/UserEdit/UserEditPage.js
+++ frontend/src/pages/UserEdit/UserEditPage.js
@@ -1,5 +1,12 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 export default function UserEditPage() {
+  const {
+    register,
+    reset,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   return <div>UserEditPage</div>;
 }
